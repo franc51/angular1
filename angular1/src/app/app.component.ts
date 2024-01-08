eae50e2db29ebf1152172adbad5ae81a325d19c3
franc51 <francisc_szasz@yahoo.com>
2024-01-08 15:20:45 +0200

    finished nav

--- angular1/src/app/app.component.ts
+++ angular1/src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
+import { CardComponent } from './card/card.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavigationComponent]
+    imports: [CommonModule, RouterOutlet, NavigationComponent, CardComponent]
 })
 export class AppComponent {
   title = 'angular1';
+
+  let data = []
+
 }
